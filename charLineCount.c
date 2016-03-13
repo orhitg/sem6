31da3d2764932a30a9114b6ea0da63bd3f794b09
orhitg <guptrrohit@gmail.com>
2016-03-13 23:37:24 +0530

    added Character and Line count

---
+++ charLineCount.c
@@ -0,0 +1,15 @@
+%{
+//calculate number of characters
+int count = 0;
+int line = 0;
+%}
+%%
+[a-zA-Z0-9] {count++;}
+\n 			{line++;}
+%%
+
+int main(){
+	yylex();
+	printf("No of id(s) found : %d and line : ",count, line);
+	return 0;
+}
